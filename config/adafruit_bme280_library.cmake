2f8afecb09d75856160baa2a6d795db48c5292e2
Sungho Park <siminsungho@gmail.com>
2021-10-15 01:23:26 +0900

    update ubinos build scripts

--- config/adafruit_bme280_library.cmake
+++ config/adafruit_bme280_library.cmake
@@ -5,6 +5,7 @@
 #
 
 set(INCLUDE__ADAFRUIT_BME280_LIBRARY TRUE)
+set(PROJECT_UBINOS_LIBRARIES ${PROJECT_UBINOS_LIBRARIES} Adafruit_BME280_Library_wrapper)
 
 set_cache_default(ADAFRUIT_BME280_LIBRARY__BASE_DIR "${PROJECT_LIBRARY_DIR}/Adafruit_BME280_Library" STRING "Adafruit BME280 Library project base dir")
 

1736701efa82e1a8be6209376975dafadbef5719
Sungho Park <siminsungho@gmail.com>
2021-10-21 11:03:03 +0900

    add clone function to ubinos config selecter

--- config/adafruit_bme280_library_nrf52840dk.cmake
+++ config/adafruit_bme280_library_nrf52840dk.cmake
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# {ubinos_config_type: [buildable, cmake, lib]}
+# ubinos_config_info {"name_base": "adafruit_bme280_library", "build_type": "cmake_ubinos"}
 
 set_cache(PROJECT_TOOLCHAIN_C_STD "GNU11" STRING)
 set_cache(PROJECT_TOOLCHAIN_CXX_STD "GNU++14" STRING)
@@ -23,8 +23,6 @@ set_cache(NRF5SDK__NRFX_UARTE0_ENABLED TRUE BOOL)
 
 set_cache(NRF5SDK__TWI_ENABLED TRUE BOOL)
 
-####
-
 include(${PROJECT_UBINOS_DIR}/config/ubinos_nrf52840dk.cmake)
 include(${PROJECT_LIBRARY_DIR}/seggerrtt_wrapper/config/seggerrtt.cmake)
 include(${PROJECT_LIBRARY_DIR}/nrf5sdk_wrapper/config/nrf5sdk.cmake)
@@ -32,10 +30,15 @@ include(${PROJECT_LIBRARY_DIR}/nrf5sdk_extension/config/nrf5sdk_extension.cmake)
 include(${PROJECT_LIBRARY_DIR}/ArduinoCore-API_wrapper/config/arduinocore_api.cmake)
 include(${PROJECT_LIBRARY_DIR}/Adafruit_BusIO_wrapper/config/adafruit_busio.cmake)
 include(${PROJECT_LIBRARY_DIR}/Adafruit_Sensor_wrapper/config/adafruit_sensor.cmake)
-include(${PROJECT_LIBRARY_DIR}/Adafruit_BME280_Library_wrapper/config/adafruit_bme280_library.cmake)
+
+include(${CMAKE_CURRENT_LIST_DIR}/adafruit_bme280_library.cmake)
+
+####
 
 string(TOLOWER ${UBINOS__BSP__BOARD_MODEL} _tmp_board_model)
+
 get_filename_component(_tmp_source_dir "${PROJECT_LIBRARY_DIR}/ArduinoCore-API_wrapper/source/arduinocore_api_ubinos/arch/arm/cortexm/${_tmp_board_model}" ABSOLUTE)
+
 include_directories(${_tmp_source_dir})
 include_directories(${_tmp_source_dir}/blank/config)
 
